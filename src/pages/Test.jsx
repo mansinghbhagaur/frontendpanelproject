97e84090a7a7f1c4b526adf07e9581d7d1fe1a88
mansinghbhagaur <mansinghbhagaur1@gmail.com>
2025-02-05 12:55:19 +0530

    test ptofile edit code

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,46 +1,104 @@
 import { Box, Button, Container, FormControl, FormLabel, Grid2, Paper, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { use } from 'react';
 
 const Test = () => {
+
+      const [open, setOpen] = useState(false);
+      const [userData, setUserData] = useState([])
+      const [inputData, setInputData] = useState({
+            name: "",
+            email: "",
+            phone: ""
+      })
+
+      const apiData = async () => {
+            await axios.get('https://jsonplaceholder.typicode.com/users/1')
+                  .then((res) => setUserData(res.data))
+      }
+      // console.log(userData.name)
+      useEffect(() => {
+            apiData()
+      }, [])
+
+
+
+      const handaleChange = (e) => {
+            const { name, value } = e.target;
+            setInputData((prev) => ({ ...prev, [name]: value }))
+            console.log(inputData)
+
+      }
+
+      const handleSubmit = (e) => {
+            e.preventDefault()
+
+
+            console.log(userData)
+            setOpen(false)
+
+      }
+
       return (
             <>
                   <Container maxWidth="sm">
+
                         <Paper elevation={10} sx={{ p: 2 }}>
                               <Typography variant='h5' textAlign='center' sx={{ pt: 2 }} gutterBottom>User Profiles</Typography>
-                              {/* <Box textAlign='center'>
-                                    <Typography variant='body1'><span style={{ fontWeight: "bold" }}>Name: </span>man singh</Typography>
+                              {!open && <Box textAlign='center'>
+                                    <Typography variant='body1'><span style={{ fontWeight: "bold" }}>Name: {userData.name}</span>man singh</Typography>
                                     <Typography variant='body1'><span style={{ fontWeight: "bold" }}>Age: </span>24</Typography>
                                     <Typography variant='body1'><span style={{ fontWeight: "bold" }}>Email: </span>test@gmail.com</Typography>
-                                    <Button sx={{ mt: 2 }} variant='contained'>Edit Profile</Button>
-                              </Box> */}
+                                    <Button sx={{ mt: 2 }} variant='contained' onClick={() => setOpen(true)}>Edit Profile</Button>
+                              </Box>
+                              }
 
-                              <Grid2 container spacing={2}>
-                                    <Grid2 size={{ xs: 12 }}>
-                                          <FormControl fullWidth>
-                                                <FormLabel htmlFor='name'>Name: </FormLabel>
-                                                <TextField fullWidth id="name" size="small" />
-                                          </FormControl>
-                                    </Grid2>
+                              {open &&
 
-                                    <Grid2 size={{ xs: 12 }}>
-                                          <FormControl fullWidth>
-                                                <FormLabel htmlFor='age'>Age: </FormLabel>
-                                                <TextField fullWidth id="age" size="small" />
-                                          </FormControl>
-                                    </Grid2>
+                                    <form onSubmit={handleSubmit}>
 
-                                    <Grid2 size={{ xs: 12 }}>
-                                          <FormControl fullWidth>
-                                                <FormLabel htmlFor='email'>Email: </FormLabel>
-                                                <TextField fullWidth id="email" size="small" />
-                                          </FormControl>
-                                    </Grid2>
 
-                                    <Grid2 size={{ xs: 12 }}>
-                                          <Button variant='outlined'>Save</Button>
-                                          <Button variant='contained' sx={{ ml: 2 }}>Cancel</Button>
-                                    </Grid2>
-                              </Grid2>
+                                          <Grid2 container spacing={2}>
+
+                                                <Grid2 size={{ xs: 12 }}>
+                                                      <FormControl fullWidth>
+                                                            <FormLabel htmlFor='name'>Name: </FormLabel>
+                                                            <TextField
+                                                                  fullWidth
+                                                                  id="name"
+                                                                  size="small"
+                                                                  name='name'
+                                                                  value={userData.name}
+                                                                  onChange={(e) => { handaleChange(e) }}
+                                                            />
+
+                                                      </FormControl>
+                                                </Grid2>
+
+                                                <Grid2 size={{ xs: 12 }}>
+                                                      <FormControl fullWidth>
+                                                            <FormLabel htmlFor='phone'>Phone: </FormLabel>
+                                                            <TextField fullWidth id="phone" size="small" name='phone' value={userData.phone} onChange={(e) => { handaleChange(e) }} />
+
+                                                      </FormControl>
+                                                </Grid2>
+
+                                                <Grid2 size={{ xs: 12 }}>
+                                                      <FormControl fullWidth>
+                                                            <FormLabel htmlFor='email'>Email: </FormLabel>
+                                                            <TextField fullWidth id="email" size="small" value={userData.email}
+                                                                  onChange={(e) => { handaleChange(e) }} />
+                                                      </FormControl>
+                                                </Grid2>
+
+                                                <Grid2 size={{ xs: 12 }}>
+                                                      <Button type='submit' variant='outlined'  >Save</Button>
+                                                      <Button variant='contained' sx={{ ml: 2 }}>Cancel</Button>
+                                                </Grid2>
+                                          </Grid2>
+                                    </form>
+                              }
                         </Paper>
                   </Container>
             </>
